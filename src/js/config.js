6ea541efc7a069c6e3b7f008692550ab91b98833
jacob-a2k <jakub.a2k@gmail.com>
2024-08-29 13:27:17 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fe0ed871-2f82-48a7-ad34-fcfae9be2c59';
+export const MODAL_CLOSE_SEC = 2.5;
